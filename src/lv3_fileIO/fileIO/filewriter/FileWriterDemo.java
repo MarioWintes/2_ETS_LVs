0b74101670fa2d2521c907ba576b4456db461083
MarioWintes <52406123@edu.campus02.at>
2025-03-12 20:13:49 +0100

    verbesserung filewriter

--- src/lv3_fileIO/fileIO/filewriter/FileWriterDemo.java
+++ src/lv3_fileIO/fileIO/filewriter/FileWriterDemo.java
@@ -1,28 +1,23 @@
 package lv3_fileIO.fileIO.filewriter;
 
 import java.io.*;
-import java.util.Objects;
-import java.util.Scanner;
 
 public class FileWriterDemo {
 
     public static void main(String[] args) throws IOException {
 
-        FileWriter fw = new FileWriter(new File("C:\\Users\\mario\\Studium\\2_ETS_LVs\\src\\lv3_fileIO\\fileIO\\filewriter\\noten.txt"));
-        BufferedWriter bw = new BufferedWriter(fw);
-
-        Scanner scanner = new Scanner(System.in);
-
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(new FileWriter("C:\\Users\\mario\\Studium\\2_ETS_LVs\\src\\lv3_fileIO\\fileIO\\filewriter\\noten.txt"))){
             String line;
 
             while (!(line = br.readLine()).equals("STOP")){
-                bw.write(line);
+                pw.write(line + "\n");
+                pw.flush();
             }
+            pw.flush();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        bw.flush();
-        bw.close();
+
     }
 }
